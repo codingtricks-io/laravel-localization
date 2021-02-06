d4b2b792058eea7cb52860dc04caa93ad5b7dd52
dinushchathurya <dinushchathurya21@gmail.com>
2021-02-06 21:48:50 +0530

    :sparkles: Create left nav n=bar component

---
+++ resources/views/layouts/inc/left_nav.blade.php
@@ -0,0 +1,12 @@
+<div class="br-logo"><a href=""><span>[</span>DevNinja<span>]</span></a></div>
+<div class="br-sideleft overflow-y-auto">
+    <label class="sidebar-label pd-x-15 mg-t-20">Navigation</label>
+    <div class="br-sideleft-menu">
+       <a href="/" class="br-menu-link active">
+           <div class="br-menu-item">
+               <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+               <span class="menu-item-label">Dashboard</span>
+           </div>
+       </a>
+    </div>
+</div>
